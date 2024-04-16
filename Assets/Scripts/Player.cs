74a3eb3dc0158fcf76ee208279ecca248320ebf1
MatthewBulow <matthewbulow@gmail.com>
2024-04-16 14:30:16 -0600

    Added ladder climbing functionality. Added bouncy mushroom tilemap. Fixed bug: wall jump fixed bug: sticking to wall Start of lesson #93

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,31 +7,43 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 1f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 1f;
     private Vector2 moveInput;
     bool playerHasHorizontalSpeed = false;
+    bool playerHasVerticalSpeed = false;
+    private float defaultGravity;
+    private float defaultClimbAnimationSpeed;
 
     private Rigidbody2D myRigidbody;
     private Animator myAnimator;
-    private CapsuleCollider2D myCollider;
+    private Collider2D myCollider;
 
     void Start()
     {
         myRigidbody = this.GetComponent<Rigidbody2D>();
         myAnimator = this.GetComponent<Animator>();
-        myCollider = this.GetComponent<CapsuleCollider2D>();
+        myCollider = this.GetComponent<BoxCollider2D>();
+
+
+        defaultGravity = myRigidbody.gravityScale;
+        defaultClimbAnimationSpeed = myAnimator.speed;
     }
 
     void Update()
     {
         playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > 0.1;
         Run();
         FlipSprite();
     }
 
+    // This method is called by the input manager
     void OnMove(InputValue value) 
     { 
         moveInput = value.Get<Vector2>();
     }
+
+    // This method is called by the input manager
     void OnJump(InputValue value)
     {
         if (value.isPressed && myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) )
@@ -42,12 +54,12 @@ public class Player : MonoBehaviour
 
     void Run()
     {
+        ClimbLadder();
+
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
-
         myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
 
-        
     }
     
     void FlipSprite()
@@ -59,5 +71,37 @@ public class Player : MonoBehaviour
         }
         
     }
+
+    void ClimbLadder()
+    {
+        
+        // if player is touching ladder layer, then set vertical velocity based on y-axis input
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidbody.gravityScale = 0;
+            Vector2 playerVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+            myRigidbody.velocity = playerVelocity;
+            if (playerHasVerticalSpeed)
+            {
+                myAnimator.speed = defaultClimbAnimationSpeed;
+                myAnimator.SetBool("isClimbing", true);
+                Debug.Log("Player has vertical speed and is touching ladder layer");
+            }
+            else
+            {
+                if (myAnimator.GetBool("isClimbing"))
+                {
+                    myAnimator.speed = 0;
+                }
+            }
+        }
+        else
+        {
+            myRigidbody.gravityScale = defaultGravity;
+            myAnimator.SetBool("isClimbing", false);
+            myAnimator.speed = defaultClimbAnimationSpeed;
+        }           
+    }
+
 
 }
